ba48292a4a57b2b899f543bcd82a0271bc0b58d3
ParasRawat29 <parasrawat29pr@gmail.com>
2022-01-05 13:17:13 +0530

    initialized the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import StateWise from "./components/StateWise";
+import Total from "./components/Total";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Covid -19 Tracker | India </h1>
       </header>
+      <main>
+        <Total />
+        <StateWise />
+      </main>
     </div>
   );
 }
---
+++ src/components/Total.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Total() {
+  return (
+    <div className="totalWrapper">
+      <div className="confirmedWrapper"></div>
+      <div className="recoveredWrapper"></div>
+      <div className="deathsWrapper"></div>
+      <div className="vaccinatedWrapper">
+        <div className="vaccinated1Wrapper"></div>
+        <div className="vaccinated2Wrapper"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Total;
