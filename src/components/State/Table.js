87d22d9aaece150a7b9301e50316465963a7e3d4
ParasRawat29 <parasrawat29pr@gmail.com>
2022-01-07 00:43:45 +0530

    implemented sorting of tables

--- src/components/State/Table.js
+++ src/components/State/Table.js
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Table({ stateData }) {
+  console.log(stateData);
+  const [currentSort, setCurrentSort] = useState("default");
+  const [sortOn, setSortOn] = useState("");
+  const sortTypes = {
+    up: {
+      class: "sort-up",
+      fun: (a, b) => {
+        // console.log(sortOn);
+        return a.total[sortOn] - b.total[sortOn];
+      },
+    },
+    down: {
+      class: "sort-down",
+      fun: (a, b) => {
+        // console.log(sortOn);
+        // console.log(a.total.sortOn - b.total.sortOn);
+        return b.total[sortOn] - a.total[sortOn];
+      },
+    },
+    default: {
+      class: "sort",
+      fun: (a, b) => a,
+    },
+  };
+
+  function sortTable() {
+    if (currentSort === "down") setCurrentSort("up");
+    else if (currentSort === "up") setCurrentSort("default");
+    else if (currentSort === "default") setCurrentSort("down");
+  }
   return (
     <table>
       <thead>
@@ -8,6 +38,18 @@ function Table({ stateData }) {
           <th className="stateNameHeading">State | Union Territory</th>
           <th className="confirmedHeading" style={{ color: "#c44938" }}>
             Confirmed
+            <button
+              onClick={() => {
+                sortTable();
+                setSortOn("confirmed");
+              }}
+            >
+              <i
+                className={`fas fa-${
+                  sortOn === "confirmed" ? sortTypes[currentSort].class : "sort"
+                }`}
+              ></i>
+            </button>
           </th>
           <th
             className="recoveredHeading"
@@ -16,28 +58,85 @@ function Table({ stateData }) {
             }}
           >
             Recovered
+            <button
+              onClick={() => {
+                sortTable();
+                setSortOn("recovered");
+              }}
+            >
+              <i
+                className={`fas fa-${
+                  sortOn === "recovered" ? sortTypes[currentSort].class : "sort"
+                }`}
+              ></i>
+            </button>
           </th>
-          <th className="deathsHeading">Deaths</th>
+          <th className="deathsHeading">
+            Deaths
+            <button
+              onClick={() => {
+                sortTable();
+                setSortOn("deceased");
+              }}
+            >
+              <i
+                className={`fas fa-${
+                  sortOn === "deceased" ? sortTypes[currentSort].class : "sort"
+                }`}
+              ></i>
+            </button>
+          </th>
+
           <th className="vaccinatedHeading" style={{ color: "#6c3d99" }}>
             Vaccinated 1
+            <button
+              onClick={() => {
+                sortTable();
+                setSortOn("vaccinated1");
+              }}
+            >
+              <i
+                className={`fas fa-${
+                  sortOn === "vaccinated1"
+                    ? sortTypes[currentSort].class
+                    : "sort"
+                }`}
+              ></i>
+            </button>
           </th>
           <th className="vaccinatedHeading" style={{ color: "#6c3d99" }}>
             Vaccinated 2
+            <button
+              onClick={() => {
+                sortTable();
+                setSortOn("vaccinated2");
+              }}
+            >
+              <i
+                className={`fas fa-${
+                  sortOn === "vaccinated2"
+                    ? sortTypes[currentSort].class
+                    : "sort"
+                }`}
+              ></i>
+            </button>
           </th>
         </tr>
       </thead>
       <tbody>
-        {stateData.map((state) => {
-          return (
-            <tr key={state.id}>
-              <td className="stateName">{state.stateName}</td>
-              <td className="stateWiseConfirmed">{state.total.confirmed}</td>
-              <td className="stateWiseRecovered">{state.total.recovered}</td>
-              <td className="stateWiseDeceased">{state.total.deceased}</td>
-              <td className="statevaccinated1">{state.total.vaccinated1}</td>
-              <td className="statevaccinated2">{state.total.vaccinated2}</td>
-            </tr>
-          );
+        {[...stateData].sort(sortTypes[currentSort].fun).map((state) => {
+          if (state.stateName !== "Total") {
+            return (
+              <tr key={state.id}>
+                <td className="stateName">{state.stateName}</td>
+                <td className="stateWiseConfirmed">{state.total.confirmed}</td>
+                <td className="stateWiseRecovered">{state.total.recovered}</td>
+                <td className="stateWiseDeceased">{state.total.deceased}</td>
+                <td className="statevaccinated1">{state.total.vaccinated1}</td>
+                <td className="statevaccinated2">{state.total.vaccinated2}</td>
+              </tr>
+            );
+          }
         })}
       </tbody>
     </table>
